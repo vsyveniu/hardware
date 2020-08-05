5c329071b29112346c6b91b852867109fc27189e
Vitaliy Syvenuk <vsyvenuk@e1r1p12.unit.ua>
2020-08-05 11:44:38 +0300

    kill build

--- t01/main/main.c
+++ t01/main/main.c
@@ -1,24 +1,21 @@
 #include "main.h"
  
-#define PIN0 33
-#define PIN1 26
-#define PIN2 27
+#define PIN_OUT_0 33
+#define PIN_OUT_1 26
+#define PIN_OUT_2 27
 
 
 void app_main(void)
 {
 
-   gpio_pad_select_gpio(PIN0);
-   gpio_pad_select_gpio(PIN1);
-   gpio_pad_select_gpio(PIN2);
-   gpio_set_direction(PIN0, GPIO_MODE_OUTPUT);
-   gpio_set_direction(PIN1, GPIO_MODE_OUTPUT);
-   gpio_set_direction(PIN2, GPIO_MODE_OUTPUT);
+  gpio_set_direction(PIN_OUT_0, GPIO_MODE_OUTPUT);
+  gpio_set_direction(PIN_OUT_1, GPIO_MODE_OUTPUT);
+  gpio_set_direction(PIN_OUT_2, GPIO_MODE_OUTPUT);
 
   while (true){
-    gpio_set_level(PIN0, 1);
-    gpio_set_level(PIN1, 1);
-    gpio_set_level(PIN2, 1);
+    gpio_set_level(PIN_OUT_0, 1);
+    gpio_set_level(PIN_OUT_1, 1);
+    gpio_set_level(PIN_OUT_2, 1);
   }
 
 }
--- t01/main/main.h
+++ t01/main/main.h
@@ -1,6 +1,10 @@
 #ifndef MAIN_H
 # define MAIN_H
 
+#include <stdio.h>
 #include "driver/gpio.h"
+#include "esp_log.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
 
 #endif
