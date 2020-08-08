ef398d3f3e23c51ed378715cdae55da80b254626
vsyveniu <vsyveniu@gmail.com>
2020-08-08 22:25:06 +0300

    t03 second interrupt

--- t03/main/main.c
+++ t03/main/main.c
@@ -4,31 +4,33 @@
 #define LED_2 	GPIO_NUM_26
 #define BUTT_1 	GPIO_NUM_39
 #define BUTT_2 	GPIO_NUM_18
-
-xQueueHandle interruptQueue1;
+#define GPIO_BUTT1_BIT_MASK (1ULL<<GPIO_NUM_39)
+#define GPIO_BUTT2_BIT_MASK (1ULL<<GPIO_NUM_18)
+ 
+xQueueHandle interruptQueue;
 xQueueHandle interruptQueue2;
 
-static void IRAM_ATTR butt1_handler(void *args){
+static void IRAM_ATTR butt_handler(void *args){
 
-	int pin = (int) args;
-	xQueueSendFromISR(interruptQueue1, &pin, NULL);
+	uint32_t pin = (uint32_t) args;
+	xQueueSendFromISR(interruptQueue, &pin, NULL);
 }
 
-static void IRAM_ATTR butt2_handler(void *args){
+ static void IRAM_ATTR butt2_handler(void *args){
 
-	int pin = (int) args;
+	uint32_t pin = (uint32_t) args;
 	xQueueSendFromISR(interruptQueue2, &pin, NULL);
-}
+}  
 
 
 
 void butt1_pushed()
 {
 	
-	int pin;
+	uint32_t pin;
 	while (true)
 	{
-		if(xQueueReceive(interruptQueue1, &pin, portMAX_DELAY))
+		if(xQueueReceive(interruptQueue, &pin, portMAX_DELAY))
 		{
 			ESP_LOGI("push", "%d button pushed %d ", pin, gpio_get_level(pin));
 			ESP_LOGI("push", "%s", "rttr");
@@ -40,7 +42,7 @@ void butt1_pushed()
 void butt2_pushed()
 {
 	
-	int pin;
+	uint32_t pin;
 	while (true)
 	{
 		if(xQueueReceive(interruptQueue2, &pin, portMAX_DELAY))
@@ -55,17 +57,17 @@ void butt2_pushed()
 void app_main(void)
 {
 	gpio_config_t butt_1_conf = {
-		.pin_bit_mask = GPIO_SEL_39,
+		.pin_bit_mask = GPIO_BUTT1_BIT_MASK,
 		.mode = GPIO_MODE_INPUT,
-		.pull_up_en = GPIO_PULLUP_ENABLE,
-		.pull_down_en = GPIO_PULLUP_DISABLE,
+		.pull_up_en = GPIO_PULLUP_DISABLE,
+		.pull_down_en = GPIO_PULLDOWN_DISABLE,
 		.intr_type = GPIO_INTR_POSEDGE,
 	};
 	gpio_config_t butt_2_conf = {
-		.pin_bit_mask = GPIO_SEL_18,
+		.pin_bit_mask = GPIO_BUTT2_BIT_MASK,
 		.mode = GPIO_MODE_INPUT,
-		.pull_up_en = GPIO_PULLUP_ENABLE,
-		.pull_down_en = GPIO_PULLUP_DISABLE,
+		.pull_up_en = GPIO_PULLUP_DISABLE,
+		.pull_down_en = GPIO_PULLDOWN_DISABLE,
 		.intr_type = GPIO_INTR_POSEDGE,
 	};
 
@@ -76,11 +78,11 @@ void app_main(void)
 	gpio_install_isr_service(0);
 
 
-	gpio_isr_handler_add(BUTT_1, butt1_handler, (void *)BUTT_1);
+	gpio_isr_handler_add(BUTT_1, butt_handler, (void *)BUTT_1);
 	gpio_isr_handler_add(BUTT_2, butt2_handler, (void *)BUTT_2);
 
 	
-	interruptQueue1 = xQueueCreate(10, sizeof(int));
+	interruptQueue = xQueueCreate(10, sizeof(int));
 	interruptQueue2 = xQueueCreate(10, sizeof(int));
 
 	 gpio_set_direction(LED_1, GPIO_MODE_OUTPUT);
@@ -89,6 +91,12 @@ void app_main(void)
 	xTaskCreate(butt1_pushed, "button 1 task", 2048, NULL, 1, NULL);
 	xTaskCreate(butt2_pushed, "button 2 task", 2048, NULL, 1, NULL);
 	//xTaskCreate(butt2_pushed);
+
+	while (true){
+		//ESP_LOGI("level-1", "%d", gpio_get_level(BUTT_1));
+
+		//ESP_LOGI("level-2", "%d", gpio_get_level(BUTT_2));
+	}
 }
 
 
