695b5f912f786579df578a97646c5e11baac817b
vsyveniu <vsyveniu@gmail.com>
2020-08-16 19:36:46 +0300

    t06

---
+++ t06/main/main.h
@@ -0,0 +1,18 @@
+#ifndef MAIN_H
+# define MAIN_H
+
+#include <stdio.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "driver/gpio.h"
+#include "esp_log.h"
+#include <esp32/rom/ets_sys.h>
+#include "esp_err.h"
+#include <esp32/rom/ets_sys.h>
+#include <string.h>
+
+#include <driver/dac.h>
+#include <driver/sigmadelta.h>
+
+
+#endif
