caa83c7b31b0de24ed58d31139820f9c50bbeb3d
Vitaliy Syvenuk <vsyvenuk@e2r3p8.unit.ua>
2020-08-27 18:23:56 +0300

    t08 interrupts

--- t08/main/main.c
+++ t08/main/main.c
@@ -81,9 +81,41 @@ int beep_count;
 
 
 
-void read_accel_data(){
+void accel_interrupt(spi_device_handle_t spi){
+	uint8_t pin;
+	int16_t x, y, z;
 
+		int8_t bufftx[3];
+		int16_t buff[3];
+	for(int i = 0; i < 8; i++){
+		buff[i] = 0x00;
+	}
+
+	spi_transaction_t adx_readData = {
+		.cmd = 0x80 | 0x40 | 0x32,
+		.length = 3 * 16,
+		.tx_buffer = bufftx,
+		.rx_buffer = buff,
+	};
+
+	while (true){
+		if(xQueueReceive(interruptQueue, &pin, portMAX_DELAY))
+		{
+			printf("%s\n", "fuck interrupt");
+			spi_device_polling_transmit(spi, &adx_readData);
+			x = buff[0];
+			y = buff[1];
+			z = buff[2];
+
+			printf("x angle  %d \n", x);
+			printf("y angle  %d \n", y);
+			printf("z angle  %d \n", z);   
+
+		}
+	}
 }
+
+
 
 
 void app_main(void){
@@ -92,7 +124,18 @@ void app_main(void){
 
 	gpio_set_direction(EN_ACCEL, GPIO_MODE_OUTPUT);
 	gpio_set_level(EN_ACCEL, 1);
-	vTaskDelay(50 / portTICK_PERIOD_MS);
+	vTaskDelay(5 / portTICK_PERIOD_MS);
+
+
+
+	gpio_config_t int_conf = {
+		.pin_bit_mask = GPIO_SEL_34,
+		.mode = GPIO_MODE_INPUT,
+		.intr_type = GPIO_INTR_POSEDGE,
+	};
+
+	gpio_config(&int_conf);
+	//gpio_set_level(SPI_INT1_PIN, 0);
 
 	spi_device_handle_t spi;
 
@@ -129,65 +172,145 @@ void app_main(void){
 	};
 
 	int16_t buff[3];
-
-	//buff[0] = 0x00;
 	for(int i = 0; i < 8; i++){
 		buff[i] = 0x00;
 	}
 
- 	spi_transaction_t spi_transaction_conf = {
-		.flags = 0x80u,
-		//.cmd = 0xff,
-		.addr= 0x00,
-		.length = 8 * 8,
-		.rxlength = 0,
-		.tx_buffer = &buff,
-		.rx_buffer = &buff,
-	};
 
-	spi_transaction_t spi_start = {
-		.flags = SPI_TRANS_USE_RXDATA,
-		.cmd = 0x00,
-		.length = 8,
-	};
+	int8_t adx_setbwbuff[1];
 
-	spi_transaction_t spi_start2 = {
+	adx_setbwbuff[0] = 0b00001010;   // set bw rate to 100hz output
+	spi_transaction_t adx_setbw = {
 		.flags = SPI_TRANS_USE_RXDATA,
-		.cmd = 0x2C,
+		.cmd = 0x2Cu,
 		.length = 8,
 	};
  
-	int16_t wbuff[3];
-	
- 	spi_transaction_t spi_write = {
+	uint8_t powctlbuff[1];
+
+	powctlbuff[0] = 0b00001011;  // set /link/measure/sleep/wakeup shit
+	spi_transaction_t adx_set_pow_ctl = {
 		.flags = SPI_TRANS_USE_RXDATA,
 		.cmd = 0x2Du,
-		.tx_buffer = wbuff,
+		.tx_buffer = powctlbuff,
 		.length = 8,
 	};
 
-	
+	uint8_t treshbuff[1];
 
-	uint16_t intbuff[3];
-
-	intbuff[0] = 230;
-
-	spi_transaction_t spi_int1 = {
-		.cmd = 0x24,
+	treshbuff[0] = 2;  //set tresh_act register
+	spi_transaction_t adx_treshhold = {
+		.cmd = 0x24u,
 		.length = 8,
-		.tx_buffer = intbuff
+		.tx_buffer = treshbuff
 	};
 
-	uint16_t intbuff_r[3];
 
-	spi_transaction_t spi_int1_r = {
-		.cmd = 0x80 | 0x40 | 0x24,
+	uint8_t dataformatbuff[1];
+
+	dataformatbuff[0] = 0b00100000;  //set dataformat
+	spi_transaction_t adx_dataformat = {
+		.cmd = 0x24u,
+		.length = 8,
+		.tx_buffer = dataformatbuff
+	};
+
+
+
+	uint8_t intbuff_r[1];
+
+	spi_transaction_t tresh_read = {
+		.cmd = 0x80 | 0x40 | 0x24u,
 		.length =  3 * 16,
 		.rx_buffer = intbuff_r,
 	};
 
-	int16_t bufftx[3];
-	spi_transaction_t spi_readData = {
+	uint8_t tresh_inact_buff[1];
+
+	tresh_inact_buff[0] = 1;   //set tresh_inact register
+	spi_transaction_t adx_inact_treshhold = {
+		.cmd = 0x25u,
+		.length = 8,
+		.tx_buffer = tresh_inact_buff
+	};
+
+	uint8_t inact_intbuff_r[3];
+
+	spi_transaction_t tresh_inact_read = {
+		.cmd = 0x80 | 0x40 | 0x25u,
+		.length =  3 * 8,
+		.rx_buffer = inact_intbuff_r,
+	};
+
+
+
+	uint8_t en_int1_buff[1];
+
+	en_int1_buff[0] = 0b00010000;  // enable interrupts on type corresponds to it's bit num and level. Activity interrupt  
+
+	spi_transaction_t adx_int1_en = {
+		.cmd = 0x2Eu,
+		.length = 8,
+		.tx_buffer = en_int1_buff
+	};
+
+
+	uint8_t en_int1buffR[1];
+
+	spi_transaction_t en1_readdata = {
+		.cmd = 0x80 | 0x40 | 0x2Eu,
+		.length = 8,
+		.rx_buffer = en_int1buffR,
+	};
+
+	uint8_t int_map_buff[1];
+
+	int_map_buff[0] = 0b11101111;   // any bits set to 0 send interrupt to INT1 pin
+	spi_transaction_t adx_int_map = {
+		.cmd = 0x2Fu,
+		.length = 8,
+		.tx_buffer = int_map_buff
+	};
+
+	uint8_t int_map_buff_R[1];
+
+	spi_transaction_t int_map_read = {
+		.cmd = 0x80 | 0x40 | 0x2F,
+		.length = 8,
+		.rx_buffer = int_map_buff_R,
+	};
+
+
+	uint8_t int_src_buff[1];
+
+	spi_transaction_t int_src = {
+		.cmd = 0x80 | 0x40 | 0x30u,
+		.length = 8,
+		.rx_buffer = int_src_buff
+	};
+
+	uint8_t act_inact_ctl[1];
+	act_inact_ctl[0] = 0b11000000;     
+
+	spi_transaction_t  adx_act_inact = {
+		.cmd = 0x27u,
+		.length = 8,
+		.tx_buffer = act_inact_ctl
+	};
+
+
+	uint8_t act_inact_read_buff[1];
+
+	spi_transaction_t act_inact_read = {
+		.cmd = 0x80 | 0x40 | 0x27u,
+		.length = 8,
+		.rx_buffer = act_inact_read_buff,
+	};
+
+	
+
+	int8_t bufftx[3];
+	spi_transaction_t adx_readData = {
 		//.flags = SPI_TRANS_USE_RXDATA,
 		.cmd = 0x80 | 0x40 | 0x32,
 		.length = 3 * 16,
@@ -195,19 +318,28 @@ void app_main(void){
 		.rx_buffer = buff,
 	};
 
-
 	err = spi_bus_initialize(SPI3_HOST, &spi_bus_conf, 0);
 	ESP_ERROR_CHECK(err);
 
 	err = spi_bus_add_device(SPI3_HOST, &spi_device_conf, &spi);
 	ESP_ERROR_CHECK(err);
 
+	///10000  int en
+	//10000011 131
+	//10010000
+	//10010011 147
+	//10010010 146 dready/activity/watermark
+	//11101111 239 map
+	//10000010 130
+	//10 2 / watermark
+	//10000 16
+	//1111000 act inact
+	// 00010010 activity / watermark
 
-	double x_angle, y_angle, z_angle, R;
 
-	int16_t x, y, z;
 
-	double pos = 0;
+	
+
 
 	gpio_install_isr_service(0);
 
@@ -215,39 +347,51 @@ void app_main(void){
 
 	interruptQueue = xQueueCreate(10, sizeof(int));
 
-	spi_device_polling_transmit(spi, &spi_start);
-	spi_device_polling_transmit(spi, &spi_start2);
-	spi_device_polling_transmit(spi, &spi_write);
 
-	spi_device_polling_transmit(spi, &spi_int1);
-	spi_device_polling_transmit(spi, &spi_int1_r);
+	xTaskCreate(accel_interrupt, "accell 1 task", 2048, spi, 1, NULL);
 
-	printf("%d treshhold\n", intbuff_r[0]);
+	spi_device_polling_transmit(spi, &adx_setbw);
+	//spi_device_polling_transmit(spi, &adx_dataformat);
+	spi_device_polling_transmit(spi, &adx_treshhold);
+	spi_device_polling_transmit(spi, &tresh_read);
+	spi_device_polling_transmit(spi, &adx_inact_treshhold);
+	spi_device_polling_transmit(spi, &tresh_inact_read);
+	spi_device_polling_transmit(spi, &adx_int1_en);
+	spi_device_polling_transmit(spi, &en1_readdata);
+	spi_device_polling_transmit(spi, &adx_int_map);
+	spi_device_polling_transmit(spi, &int_map_read);
+	spi_device_polling_transmit(spi, &adx_act_inact);
+	spi_device_polling_transmit(spi, &act_inact_read);
+
+	printf("%d treshhold act\n", intbuff_r[0]);
+	printf("%d treshhold inact\n", tresh_inact_buff[0]);
+/* 	printf("%d interrupt en \n", en_int1buffR[0]);
+	printf("%d interrupt map \n", int_map_buff_R[0]);
+	printf("%d act inact \n", act_inact_read_buff[0]); */
+	spi_device_polling_transmit(spi, &adx_set_pow_ctl);
 
 	while (true)
 	{
 		//printf("%s\n", "----------");
 		//vTaskDelay(200 / portTICK_PERIOD_MS);
-		//spi_device_polling_transmit(spi, &spi_transaction_conf_start);
+		//spi_device_polling_transmit(spi, &adx_readData);
 
-		//vTaskDelay(2000 / portTICK_PERIOD_MS);
+		spi_device_polling_transmit(spi, &int_src);
+		//if(int_src_buff[0] == 18 || int_src_buff[0] == 146)
+			//printf("%d interrupt src \n", int_src_buff[0]);
+			printf("%d\n", gpio_get_level(SPI_INT1_PIN));
 
 
-		spi_device_polling_transmit(spi, &spi_readData);
-		spi_device_polling_transmit(spi, &spi_start);
 
 
-		x = buff[0];
-		y = buff[1];
-		z = buff[2];
 
 		/* x_angle = atan2(y, z) * 180/M_PI;
 		y_angle = atan2(x, z) * 180/M_PI;
 		z_angle = atan2(y, x) * 180/M_PI; */
 
-	 	printf("x angle  %d \n", x);
-		printf("y angle  %d \n", y);
-		printf("z angle  %d \n", z);
+	 	  // printf("x angle  %d \n", x);
+		//printf("y angle  %d \n", y);
+//		printf("z angle  %d \n", z);   
 
 
 		/* if((x > 230 || y > 230 || z > 230) || (x < -230 || y < -230 || z < -230)){
